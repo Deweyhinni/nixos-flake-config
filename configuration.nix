5211487ea7a02fec5282021e33112444dc7dea93
deweyhinni <deweyhinni@protonmail.com>
2025-05-05 01:27:56 +0000

    pipewire

--- configuration.nix
+++ configuration.nix
@@ -49,6 +49,16 @@
     variant = "";
   };
 
+  # Enable sound with pipewire
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.deweyhinni = {
     isNormalUser = true;
