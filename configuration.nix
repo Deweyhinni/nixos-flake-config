d2a9162a0a26cfd602b4dbe5179940c028722be5
deweyhinni <deweyhinni@protonmail.com>
2025-05-31 02:43:09 +0000

    boot splash screen and stuff

--- configuration.nix
+++ configuration.nix
@@ -17,16 +17,47 @@
   boot.loader.grub.useOSProber = true;
   */
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
 
-  boot.initrd.luks.devices."luks-14d0c9f6-f237-4c37-95d1-2f0e0a034279".device = "/dev/disk/by-uuid/14d0c9f6-f237-4c37-95d1-2f0e0a034279";
+    # Bootloader.
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.kernelModules = [ "amdgpu" ];
+    # Use latest kernel.
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    initrd.luks.devices."luks-14d0c9f6-f237-4c37-95d1-2f0e0a034279".device = "/dev/disk/by-uuid/14d0c9f6-f237-4c37-95d1-2f0e0a034279";
+
+    initrd.kernelModules = [ "amdgpu" ];
+
+    initrd.availableKernelModules = [ "amdgpu" ];
+
+    initrd.systemd.enable = true;
+
+    plymouth = {
+      enable = true;
+      theme = "circle";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes)
+      ];
+    };
+
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+
+    loader.timeout = 0;
+  };
+
+  systemd.user.extraConfig = "DefaultTimeoutStopSec=10s";
+  systemd.extraConfig = "DefaultTimeoutStopSec=10s";
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
