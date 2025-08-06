e2e3ba0bc9cd2e97c0800e39a922de33453eacdc
deweyhinni <deweyhinni@protonmail.com>
2025-08-06 18:07:51 +0000

    gnome keybinds and stuff

--- desktops/gnome.nix
+++ desktops/gnome.nix
@@ -18,5 +18,42 @@
     gnomeExtensions.wireless-hid
     gnomeExtensions.wallpaper-slideshow
     gnomeExtensions.removable-drive-menu
+    dconf-editor
   ];
+
+  home-manager.users.deweyhinni = {
+    dconf = {
+      enable = true;
+      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      settings."org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = with pkgs.gnomeExtensions; [
+          blur-my-shell.extensionUuid
+          pop-shell.extensionUuid
+          just-perfection.extensionUuid
+          hide-minimized.extensionUuid
+        ];
+      };
+      settings."org/gnome/desktop/wm/keybindings" = {
+        switch-to-workspace-1 = ["<Super>1"];
+        switch-to-workspace-2 = ["<Super>2"];
+        switch-to-workspace-3 = ["<Super>3"];
+        switch-to-workspace-4 = ["<Super>4"];
+        switch-to-workspace-5 = ["<Super>5"];
+        switch-to-workspace-6 = ["<Super>6"];
+
+        move-to-workspace-1 = ["<Super><Shift>1"];
+        move-to-workspace-2 = ["<Super><Shift>2"];
+        move-to-workspace-3 = ["<Super><Shift>3"];
+        move-to-workspace-4 = ["<Super><Shift>4"];
+        move-to-workspace-5 = ["<Super><Shift>5"];
+        move-to-workspace-6 = ["<Super><Shift>6"];
+
+        switch-to-workspace-left = ["<Control><Super>h"];
+        switch-to-workspace-right = ["<Control><Super>l"];
+        switch-to-workspace-down = ["<Control><Super>j"];
+        switch-to-workspace-up = ["<Control><Super>k"];
+      };
+    };
+  };
 }
