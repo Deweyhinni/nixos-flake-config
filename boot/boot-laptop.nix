820e03113f89deac07376bba81d70fca6f5538f8
deweyhinni <deweyhinni@protonmail.com>
2026-04-20 15:18:26 +0000

    laptop boot config limit

--- boot/boot-laptop.nix
+++ boot/boot-laptop.nix
@@ -6,6 +6,8 @@
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
 
+    loader.systemd-boot.configurationLimit = 10;
+
     # Use latest kernel.
     kernelPackages = pkgs.linuxPackages_latest;
 
